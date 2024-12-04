3c9c02edb0b9106b7a071e28a4a92308dc428478
moulaimen <votre_email>
2024-12-04 20:39:14 +0100

    fix : fix style

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,67 @@ import 'package:timetable_app/HomePage.dart';
 import 'package:timetable_app/login_page.dart';
 import 'package:timetable_app/register_page.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 final routes = {
-  '/login': (BuildContext context) => new LoginPage(),
-  '/home': (BuildContext context) => new HomePage(),
-  '/register': (BuildContext context) => new RegisterPage(),
-  '/': (BuildContext context) => new LoginPage(),
+  '/login': (BuildContext context) => LoginPage(),
+  '/home': (BuildContext context) => HomePage(),
+  '/register': (BuildContext context) => RegisterPage(),
+  '/': (BuildContext context) => LoginPage(),
 };
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: 'Login Register App',
-      theme: new ThemeData(primarySwatch: Colors.teal),
+      theme: ThemeData(
+        // Modification de la couleur principale en bleu
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: TextTheme(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+            color: Colors.blue[800], // Couleur des titres en bleu
+          ),
+          bodyMedium: TextStyle(
+            fontSize: 18,
+            color: Colors.black87, // Couleur du texte principal
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue), // Couleur bleu pour le focus
+          ),
+          labelStyle: TextStyle(color: Colors.blue[600]), // Couleur du label en bleu
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue, // Couleur des boutons en bleu
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 25),
+            textStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          color: Colors.blue[600], // Couleur du AppBar en bleu
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+            color: Colors.white,
+          ),
+        ),
+      ),
       routes: routes,
     );
   }
